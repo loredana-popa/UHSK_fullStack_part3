55e1587f81e1f69ce65bd66a1d274e2ab7e9ee6b
loredana-popa <loredana.iacob87@gmail.com>
2022-09-12 12:36:59 +0300

    3.4: Phonebook backend step4

--- the-phonebook-backend/index.js
+++ the-phonebook-backend/index.js
@@ -36,15 +36,18 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  console.log(typeof id)
   const person = persons.find(person => person.id === id)
-  console.log(person)
   if (person) {
     response.json(person)
   } else {
     response.status(404).end()
   }
-  
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
 })
 
 const entriesNr =  persons.length
