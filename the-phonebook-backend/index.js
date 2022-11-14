7b79dcbac5e3ac5ac96ab77506b9448772c94811
loredana-popa <loredana.iacob87@gmail.com>
2022-11-14 19:33:40 +0200

    3.17*: Phonebook database, step5

--- the-phonebook-backend/index.js
+++ the-phonebook-backend/index.js
@@ -101,6 +101,23 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  console.log('body is', body)
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.get('/info', (request, response) => {
   const entriesNr =  persons.length
   const date = new Date()
